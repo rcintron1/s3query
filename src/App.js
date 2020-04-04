28dd31fd3efa88476517b1479dcedc48569da99b
Rolando.Cintron <rolando.cintron@gmail.com>
2020-04-04 17:20:51 -0400

    feat: added MFA option

--- src/App.js
+++ src/App.js
@@ -1,24 +1,61 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AWS from 'aws-sdk'
+
+
+const s3 = new AWS.S3
+const sts = new AWS.STS
 
 const {app} = window.require('electron').remote;
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>React + Electron = <span role="img" aria-label="love">😍</span></h2>
-        </div>
-        <p className="App-intro">
-          <b> Release 0.2.7 </b>
-          Version: {app.getVersion()}
-        </p>
+const getCreds = () => {
+  
+}
+
+const Login = (props)=> {
+  const [mfaKey, setMfaKey]= useState()
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    // get temporary tokens
+
+    setMfaKey('');
+  };
+  
+  return (
+    <form onSubmit={handleSubmit}>
+      <input 
+        type="text" 
+        value={mfaKey}
+        onChange={event => setMfaKey(event.target.value )}
+        placeholder="MultiFactor Key" 
+        required 
+      />
+      <button>Authenticate with MFA</button>
+    </form>
+  );
+}
+
+
+const S3FileList = (props)=>{
+  return <div><h1>list of files</h1></div>
+}
+
+const App = () => {
+  const [awsCreds, setawsCreds] = useState()
+  return (
+    <div className="App">
+      <div className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <h2>Clarabridge</h2>
       </div>
-    );
-  }
+      {awsCreds?null:<Login setcreds={setawsCreds}/>}
+      <p className="App-intro">
+        <b> Release 0.2.7 </b>
+        Version: {app.getVersion()}
+      </p>
+    </div>
+  );
 }
 
 export default App;
